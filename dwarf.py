aca31927bad5d0ac605247ee1aa5f3c99415155b
BertBash <34818466+BertBash@users.noreply.github.com>
2020-12-16 19:10:53 -0600

    Added support for bijection of n-grams.

--- dwarf.py
+++ dwarf.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 
 def translate(eng, key0, key1):
-    dwf = ''
-    for l in eng:
-        if (l >= 'a' and l <= 'z'):
-            i = key0.index(l.lower())
-            dwf += key1[i]
-        else:
-            dwf += l
+    dwf =''
+    i = 0
+    j = 0
+
+    while i < len(eng):
+        while j < len(key0):
+            t = key0[j]
+            l = eng[i:i+len(t)].lower()
+            if (l[0] >= 'a' and l[0] <= 'z'):
+                if l == t:
+                    #print("Matched!", t,l)
+                    dwf += key1[j]
+                    j = len(key0)
+            else:
+                dwf += l
+                j = len(key0)
+            j += 1
+        j = 0
+        i += len(t)
     return dwf
+
+
 
 translationFile = 'dwarfKey.txt'
 key0 = [' ']
